69f8496c9dd495e530de7a5c65d7b5c0b83f27f5
Andrew Ford <adhesionmusic@gmail.com>
2012-12-17 02:57:05 -0500

    door fix

--- Door.js
+++ Door.js
@@ -4,13 +4,18 @@ var Door = me.ObjectEntity.extend({
     {
         settings = settings || {};
         settings.image        = settings.image || me.loader.getImage( settings.width > settings.height ? "hdoor" : "vdoor" );
+        settings.spritewidth = settings.width > settings.height ? 192 : 96;
+        settings.spriteheight = settings.width > settings.height ? 96 : 192;
+        console.log ( settings.width, settings.height );
 
         if( ! settings.require ) {
             throw "Must set required count on door";
         }
 
-
         this.parent( x, y, settings );
+
+        this.addAnimation( "play", [ 0, 1, 2 ] );
+        this.setCurrentAnimation( "play" );
 
         this.door = 1;
         this.gravity = 0;
